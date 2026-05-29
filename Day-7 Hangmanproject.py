64db29184dd0bb5834ed7205cd41e83baea91dc4
AmazingmantisN <snadhish717@gmail.com>
2026-05-29 10:54:57 -0500

    Black jack projectt over

--- Day-7 Hangmanproject.py
+++ Day-7 Hangmanproject.py
@@ -1,13 +1,76 @@
-import random
+
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ["aardvark", "baboon", "camel"]
 
-chosen_word = random.choice(word_list)
-print(chosen_word)
+# TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
+#  Set 'lives' to equal 6.
 
-placeholder = []
-word_length = len(chosen_word)
+import random
+word_list = ["aardvark", "baboon", "camel"]
+lives = 6
+
+chosen_word = random.choice(word_list);
+print(chosen_word);
+
+placeholder = [];
+word_length = len(chosen_word);
 for position in range(word_length):
-    placeholder.append("_")
+    placeholder.append("_");
 print(placeholder)
 
 
@@ -29,10 +92,19 @@ while (correct == False):
         if guess == chosenwordlist[i]:
             placeholder[i] = chosenwordlist[i];
 
+    if guess not in chosenwordlist:
+        lives -= 1;
+
 
 
     for j in range(word_length):
         print(placeholder[j], end="");
 
+    print(stages[lives]);
+
+
+    if lives == 0:
+        print("You lose, the man died");
+        correct = True;
     if placeholder == chosenwordlist:
-        correct = True
+        correct = True;
